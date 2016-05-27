6296baff14e8537d4e19c6fedeb20168763ea071
xiaoyan66 <1587948529@qq.com>
2016-05-27 16:05:13 +0800

    Initial commit

---
+++ app/src/main/java/com/ralph/second/Ex26Activity.java
@@ -0,0 +1,143 @@
+package com.ralph.second;
+
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by 果占先 on 2016/5/26.
+ */
+public class Ex26Activity extends AppCompatActivity {
+    TextView txt;
+
+    PopupMenu popm;
+
+    ActionBar ab;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ex26_layout);
+        txt = (TextView) findViewById(R.id.txt);
+        registerForContextMenu(txt);
+//        ab = getActionBar();
+//        ab.setDisplayShowHomeEnabled(false);
+//        ab.setDisplayHomeAsUpEnabled(true);
+//        ab.setTitle("");
+        Toolbar toolbar= (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("we");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inf =new MenuInflater(this);
+        inf.inflate(R.menu.ex26menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.font_10:
+                txt.setTextSize(10*3);
+                break;
+            case R.id.font_12:
+                txt.setTextSize(12*3);
+                break;
+            case R.id.font_14:
+                txt.setTextSize(14*3);
+                break;
+            case R.id.font_16:
+                txt.setTextSize(16*3);
+                break;
+            case R.id.normal_btn:
+                Toast.makeText(Ex26Activity.this, "肖岩作业", Toast.LENGTH_SHORT).show();
+                break;
+            case android.R.id.home:
+                Toast.makeText(Ex26Activity.this, "点击了Home图标", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        menu.add(0,0x211,0,"红色");
+        menu.add(0,0x212,1,"绿色");
+        menu.add(0,0x213,2,"蓝色");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case 0x211:
+                txt.setTextColor(Color.RED);
+                break;
+            case 0x212:
+                txt.setTextColor(Color.GREEN);
+                break;
+            case 0x213:
+                txt.setTextColor(Color.BLUE);
+                break;
+        }
+        return true;
+    }
+
+    public void changeFont(View view)
+    {
+        popm = new PopupMenu(this,view);
+        getMenuInflater().inflate(R.menu.ex26menu,popm.getMenu());
+        popm.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.font_10:
+                        txt.setTextSize(10*3);
+                        break;
+                    case R.id.font_12:
+                        txt.setTextSize(12*3);
+                        break;
+                    case R.id.font_14:
+                        txt.setTextSize(14*3);
+                        break;
+                    case R.id.font_16:
+                        txt.setTextSize(16*3);
+                        break;
+                    case R.id.normal_btn:
+                        Toast.makeText(Ex26Activity.this, "肖岩作业", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
+
+        popm.show();
+    }
+
+    public void showActionBar(View view)
+    {
+        ab.show();
+    }
+    public void hideActionBar(View view)
+    {
+        ab.hide();
+    }
+}
---
+++ app/src/main/java/com/ralph/second/Ex27Activity.java
@@ -0,0 +1,77 @@
+package com.ralph.second;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+
+/**
+ * Created by Administrator on 2016/5/27.
+ */
+public class Ex27Activity extends Activity implements View.OnTouchListener{
+    private ImageView myImageView;
+    private int screenWidth;
+    private int screenHeight;
+    private int X,Y;
+    private void img() {
+        this.myImageView = (ImageView) this.findViewById(R.id.imgView);
+        this.myImageView.setOnTouchListener(this);
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        screenWidth = dm.widthPixels;
+        screenHeight = dm.heightPixels;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity01);
+        this.img();
+    }
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                X = (int) event.getRawX();
+                Y = (int) event.getRawY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int dx = (int) event.getRawX() - X;
+                int dy = (int) event.getRawY() - Y;
+
+                int left = v.getLeft() + dx;
+                int top = v.getTop() + dy;
+                int right = v.getRight() + dx;
+                int bottom = v.getBottom() + dy;
+
+                if (left < 0) {
+                    left = 0;
+                    right = left + v.getWidth();
+                }
+
+                if (right > screenWidth) {
+                    right = screenWidth;
+                    left = right - v.getWidth();
+                }
+
+                if (top < 0) {
+                    top = 0;
+                    bottom = top + v.getHeight();
+                }
+
+                if (bottom > screenHeight) {
+                    bottom = screenHeight;
+                    top = bottom - v.getHeight();
+                }
+                v.layout(left, top, right, bottom);
+
+                X = (int) event.getRawX();
+                Y = (int) event.getRawY();
+
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+        }
+        return true;
+    }
+}
+
